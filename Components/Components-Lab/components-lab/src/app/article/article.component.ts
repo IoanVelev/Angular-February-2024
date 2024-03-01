5d349fbf49bd6a183a00a250b9442b358c59aa5f
Ioan Velev <ioanvelev46@gmail.com>
2024-03-01 16:14:14 +0200

    Create needed properties and methods

--- Components/Components-Lab/components-lab/src/app/article/article.component.ts
+++ Components/Components-Lab/components-lab/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
+  private symbols: number = 250;
+  @Input() article!: Article;
+  @Input() articleDesc: string = '';
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show image';
 
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+
+
+  readMore() {
+    this.articleDescLen = this.symbols;
+    this.descToShow += this.article.description.substring(0, this.articleDescLen);
+
+
+    if (this.descToShow.length >= this.article.description.length) {
+      this.showReadMoreBtn = !this.showReadMoreBtn;
+      this.showHideBtn = !this.showHideBtn;
+    }
+  }
+
+  hideDesc() {
+    this.descToShow = '';
+    
+    this.showReadMoreBtn = !this.showReadMoreBtn;
+    this.showHideBtn = !this.showHideBtn;
+  }
+
+  toggleImage() {
+    this.imageIsShown = !this.imageIsShown;
+
+    if (this.imageIsShown) {
+      this.imageButtonTitle = 'Hide image';
+    } else {
+      this.imageButtonTitle = 'Show image';
+    }
+  }
 }
