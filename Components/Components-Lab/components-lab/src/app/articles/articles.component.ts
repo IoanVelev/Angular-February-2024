53befd89626348640af522f150ba24fd6bd250df
Ioan Velev <ioanvelev46@gmail.com>
2024-03-01 15:32:12 +0200

    Visualize articles

--- Components/Components-Lab/components-lab/src/app/articles/articles.component.ts
+++ Components/Components-Lab/components-lab/src/app/articles/articles.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+import { ArticleData } from '../data/data';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+articles !: Article[];
 
+constructor(){
+  //TODO
+}
+
+ngOnInit() {
+  this.articles = new ArticleData().getData();
+}
 }
--- Components/Components-Lab/components-lab/src/app/data/data.ts
+++ Components/Components-Lab/components-lab/src/app/data/data.ts
@@ -2,13 +2,13 @@ import { Article } from "../models/article.model";
 import { data } from "./seed";
 
 export class ArticleData {
-    getData(): Article[] {
+     getData(): Article[] {
         let articles: Article[] = [];
 
         data.map(article => articles.push(new Article(article.title, article.description, article.author, article.imageUrl)));
 
 
-        return articles
+        return articles;
         
     }
 }
