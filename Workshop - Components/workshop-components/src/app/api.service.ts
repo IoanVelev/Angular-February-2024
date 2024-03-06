d8ad1c101bd02124b0ca976ebf91929802ad4fb5
Ioan Velev <ioanvelev46@gmail.com>
2024-03-06 23:15:16 +0200

    Add api url in environment file

--- Workshop - Components/workshop-components/src/app/api.service.ts
+++ Workshop - Components/workshop-components/src/app/api.service.ts
@@ -1,9 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  
 
-  constructor() { }
+  constructor(private http: HttpClientModule) {
+    const { apiUrl } = environment
+  }
+
+  getThemes(){
+
+  }
+
+  getPosts(limit?:number){
+
+  }
 }
