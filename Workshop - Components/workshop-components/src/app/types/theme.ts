fcd3f0b8b5d18e1c43199dd93355b82dc044066a
Ioan Velev <ioanvelev46@gmail.com>
2024-03-08 19:47:39 +0200

    Create interface for user

--- Workshop - Components/workshop-components/src/app/types/theme.ts
+++ Workshop - Components/workshop-components/src/app/types/theme.ts
@@ -1,21 +1,12 @@
+import { User } from "./user"
+
 export interface Theme {
-    subscribers: string[],
-    posts: string[],
-    _id: string,
-    themeName: string,
-    userId: {
-        themes: string[],
-        posts: string[],
-        _id: string,
-        tel: string,
-        email: string,
-        username: string,
-        password: string,
-        created_at: string,
-        updatedAt: string,
-        __v: number
-    },
-    created_at: string,
-    updatedAt: string,
-    __v: number
+    subscribers: string[];
+    posts: string[];
+    _id: string;
+    themeName: string;
+    userId: User;
+    created_at: string;
+    updatedAt: string;
+    __v: number;
 }
