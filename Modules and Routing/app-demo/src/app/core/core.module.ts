d56c45e125400cb9df654e2590229a3d3a9b63ae
Ioan Velev <ioanvelev46@gmail.com>
2024-03-12 22:12:42 +0200

    Create global-loader component in core module

--- Modules and Routing/app-demo/src/app/core/core.module.ts
+++ Modules and Routing/app-demo/src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GlobalLoaderComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [GlobalLoaderComponent]
 })
 export class CoreModule { }
