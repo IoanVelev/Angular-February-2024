8ce9ffb41f8f52bc84ff9be847094345075d2e78
Ioan Velev <ioanvelev46@gmail.com>
2024-03-12 22:19:43 +0200

    Import shared module in core module

--- Modules and Routing/app-demo/src/app/app.module.ts
+++ Modules and Routing/app-demo/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     CoreModule
   ],
   providers: [],
