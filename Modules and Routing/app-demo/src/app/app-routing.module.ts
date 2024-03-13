50894ec73f0f949162c4239f33d853086a1f04dd
Ioan Velev <ioanvelev46@gmail.com>
2024-03-13 15:31:24 +0200

    Generate component and add it to routes

--- Modules and Routing/app-demo/src/app/app-routing.module.ts
+++ Modules and Routing/app-demo/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { HomeViewComponent } from './core/home-view/home-view.component';
 
-const routes: Routes = [{ path: '', component: TodoListComponent},
-{ path: 'user-list', component: UserListComponent}
+const routes: Routes = [{ path: '', component: HomeViewComponent },
+{ path: 'user-list', component: UserListComponent },
+{ path: 'todo-list', component: TodoListComponent }
 ];
 
 
--- Modules and Routing/app-demo/src/app/core/core.module.ts
+++ Modules and Routing/app-demo/src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeViewComponent } from './home-view/home-view.component';
 
 
 
 @NgModule({
   declarations: [
-    GlobalLoaderComponent
+    GlobalLoaderComponent,
+    HomeViewComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
-  exports: [GlobalLoaderComponent]
+  exports: [GlobalLoaderComponent, HomeViewComponent]
 })
 export class CoreModule { }
