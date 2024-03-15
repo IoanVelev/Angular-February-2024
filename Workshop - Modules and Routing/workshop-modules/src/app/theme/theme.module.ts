6773b2e563ef812beb410df3db96f918f5310253
Ioan Velev <ioanvelev46@gmail.com>
2024-03-15 19:25:47 +0200

    Generate current-theme and add-theme

--- Workshop - Modules and Routing/workshop-modules/src/app/theme/theme.module.ts
+++ Workshop - Modules and Routing/workshop-modules/src/app/theme/theme.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddThemeComponent } from './add-theme/add-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddThemeComponent,
+    CurrentThemeComponent
+  ],
   imports: [
     CommonModule
   ]
