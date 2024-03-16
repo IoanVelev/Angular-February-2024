162259aad8d8394fdf5c58ef8f372a068321dcfc
Ioan Velev <ioanvelev46@gmail.com>
2024-03-16 22:27:27 +0200

    Change app architecture

--- Workshop - Modules and Routing/workshop-modules/src/app/app.module.ts
+++ Workshop - Modules and Routing/workshop-modules/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { MainComponent } from './main/main.component';
-import { ThemesListComponent } from './themes-list/themes-list.component';
-import { PostsListComponent } from './posts-list/posts-list.component';
+import { MainComponent } from './theme/main/main.component';
+import { ThemesListComponent } from './theme/themes-list/themes-list.component';
+import { PostsListComponent } from './theme/posts-list/posts-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
@@ -16,9 +16,6 @@ import { ThemeModule } from './theme/theme.module';
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    ThemesListComponent,
-    PostsListComponent,
     HomeComponent,
   ],
   imports: [
--- Workshop - Modules and Routing/workshop-modules/src/app/theme/theme.module.ts
+++ Workshop - Modules and Routing/workshop-modules/src/app/theme/theme.module.ts
@@ -4,18 +4,25 @@ import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
 import { RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     AddThemeComponent,
-    CurrentThemeComponent
+    CurrentThemeComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent,
   ],
   imports: [
     CommonModule,
     ThemeRoutingModule,
-    RouterModule
-  ]
+    RouterModule,
+    SharedModule  ]
 })
 export class ThemeModule {}
