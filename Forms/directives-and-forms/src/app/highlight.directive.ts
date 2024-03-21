95d2c926b21e4e620cd177a2c8fc1c0adb4cd368
Ioan Velev <ioanvelev46@gmail.com>
2024-03-21 19:39:45 +0200

    Modify DOM through ElementRef and Renderer2

--- Forms/directives-and-forms/src/app/highlight.directive.ts
+++ Forms/directives-and-forms/src/app/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit{
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    //Bad way
+    this.elRef.nativeElement.style.background = 'grey';
+
+    //Good way
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'green');
+    
+  }
 
 }
